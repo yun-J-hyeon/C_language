3957afdf0dbf654bc8f7af4f5bf5195931b806ac
yun-J-hyeon <yjh9058@gmail.com>
2023-05-15 23:42:35 +0900

    202337028, 게임 코드

--- homework01/main.c
+++ homework01/main.c
@@ -1,6 +1,43 @@
 #include <stdio.h>
+#include "system_screen.h"
 
 int main()
 {
+	title();
+
+	int game_state = 1;
+	int input = 0;
+
+	while(game_state)
+	{
+		printf("input(1~3) : ");
+		scanf("%d",&input);
+		if(input==3)
+		{
+			game_state=0;
+		}
+
+		else if(input==2)
+		{
+			how_to_play();
+			int htp = 1;
+			while(htp)
+			{
+				printf("Return to menu? (1.yes 2.no) : ");
+				scanf("%d",&input);
+				if(input==1)
+				{
+					htp = 0;
+					title();
+				}
+				else if(input==2)
+				{
+					how_to_play();
+				}
+			}
+		}
+
+	}
+
     return 0;
 }
--- homework01/system_screen.c
+++ homework01/system_screen.c
@@ -1,2 +1,65 @@
 #include "system_screen.h"
 #include <stdio.h>
+
+int title()
+{
+    printf("##################################################")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##################################################")
+
+    return 0;
+}
+
+int how_to_play()
+{
+    printf("##################################################")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##################################################")
+
+    return 0;
+}
+
+int game_over()
+{
+    printf("##################################################")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##                                              ##")
+    printf("##################################################")
+
+    return 0;
+}
